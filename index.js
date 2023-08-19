25db61ecbace765fd3c482f956dbbf2e98ec2c93
akramovasadbek <fifaandothergames01@gmail.com>
2023-08-19 19:57:14 +0500

    deleted socketr

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 const express = require("express");
 const fileupload = require("express-fileupload");
-const http = require("http");
-const socketIo = require("socket.io");
 const cors = require("cors");
 const mongoose = require("mongoose"); 
 const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express()
-const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    // origin: "*",
-    origin: "*",
-    // methoda: ["GET", "POST"],
-  }
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(fileupload({ useTempFiles: true }));
-
-app.get("/", (req, res) => {
-  res.send("Webstar Team")
-})
 
 const PORT = process.env.PORT || 4005;
 const MONGO_URL = process.env.MONGO_URL;
@@ -35,6 +20,6 @@ async function start() {
     useUnifiedTopology: true,
     family: 4
   });
-  server.listen(PORT, () => {console.log(`Server listened on port: ${PORT}`)})
+  app.listen(PORT, () => {console.log(`Server listened on port: ${PORT}`)})
 }
 start()
