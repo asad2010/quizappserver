663871428c6f9f9f7c04d66871f2d0ef251cf2c7
akramovasadbek <fifaandothergames01@gmail.com>
2023-09-02 18:22:39 +0500

    of1.c

--- src/controller/questionCtrl.js
+++ src/controller/questionCtrl.js
@@ -30,7 +30,7 @@ const questionCtrl = {
             const category = await Categories.findOne({_id: newQuestion.category})
             if(!category) return res.status(404).send({message: "Category not found"})
             await Categories.findOneAndUpdate({_id: newQuestion.category},{$push: {questions: newQuestion}})
-            res.send({ message: "Question created successfully" , newQuestion})
+            res.status(201).send({ message: "Question created successfully" , newQuestion})
         } catch (error) {
             console.error(error)
             res.status(500).send({ message: "Something went wrong" })
@@ -39,20 +39,19 @@ const questionCtrl = {
     delQuestion: async (req, res) => {
         const { id } = req.params;
         try {
-            const categoryOne = await Categories.find();
-            const category = await Categories.findOne({_id: {$in: categoryOne}})
-            console.log(category)
+            const categories = await Categories.find();
+            const category = await Categories.findOne({_id: {$in: categories}})
+            if(!category) return res.status(404).send({message: "Category not found"})
             const deleteQuestion = category.questions
             if(deleteQuestion==id){
                 await Categories.updateOne(
                     { _id: category._id},
                     { $pull: { questions: id }} 
-                  )
-            }else {
+                    )
+                res.status(200).send({ message: "Question deleted successfully" })
+            } else {
                 res.status(403).send({message: "Category don't have this question"})
             }
-            if(!category) return res.status(404).send({message: "Category not found"})
-            res.status(200).send({ message: "Question deleted successfully" })
         } catch (error) {
             console.error(error)
             res.status(500).send({ message: "Something went wrong..." })
