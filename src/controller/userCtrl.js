0ba78ea64bb8689da0e4a187a429951bf32999df
akramovasadbek <fifaandothergames01@gmail.com>
2023-09-29 20:22:26 +0500

    f

--- src/controller/userCtrl.js
+++ src/controller/userCtrl.js
@@ -1,14 +1,13 @@
 const Users = require("../model/userModel");
 const { uploadFile, deleteFile } = require("../services/cloudinary")
-const bcrypt = require('bcrypt') 
 const userCtrl = {
     viewProfile: async (req, res) => {
         try {
             const { id } = req.params;
             const user = await Users.findById(id)
-            if(!user) return res.status(404).send({message: "User not found!"})
+            if (!user) return res.status(404).send({ message: "User not found!" })
             const { password, ...otherData } = user._doc
-            res.status(200).send({user: otherData})
+            res.status(200).send({ user: otherData })
         } catch (error) {
             console.error(error)
             res.status(500).send({ message: "Something went wrong..." })
@@ -38,13 +37,13 @@ const userCtrl = {
         try {
             const user = await Users.findById(id)
             if (!user) return res.status(404).send({ message: "User not found" })
-            
-            const {email} = req.body;
-            const isExists = await Users.findOne({email})
-            if(isExists) return res.status(402).send({message: "User already exists"})
+
+            const { email } = req.body;
+            const isExists = await Users.findOne({ email })
+            if (isExists) return res.status(402).send({ message: "User already exists" })
             // update profile img
             if (req.files) {
-                const {profilePicture} = req.files
+                const { profilePicture } = req.files
                 if (profilePicture.mimetype == "image/png" || profilePicture.mimetype == "image/jpeg") {
                     const result = await uploadFile(profilePicture)
                     req.body.profilePicture = result;
@@ -54,7 +53,7 @@ const userCtrl = {
                 } else {
                     return res.status(400).json({ message: "File format is should png or jpeg!" })
                 }
-            }            
+            }
             const updatedUser = await Users.findByIdAndUpdate(id, req.body, { new: true })
             const { password, ...otherDetails } = updatedUser._doc
             res.status(200).send({ message: "User successfully updated", user: otherDetails })
@@ -68,42 +67,66 @@ const userCtrl = {
         try {
             const user = await Users.findByIdAndDelete(id)
             if (!user) return res.status(404).send({ message: "User not found" })
-            
-            if (user.profilePicture.public_id) { 
+
+            if (user.profilePicture.public_id) {
                 await deleteFile(user.profilePicture.public_id)
-            }            
-            res.status(200).send({message: "User deleted successfully!"})
+            }
+            res.status(200).send({ message: "User deleted successfully!" })
         } catch (error) {
             res.status(500).send({ message: "Something went wrong..." })
             console.error(error)
         }
     },
-    allowUser: async (req,res) => {
+    allowUser: async (req, res) => {
         try {
-            const {id} = req.params;
+            const { id } = req.params;
 
-            const user = await Users.findByIdAndUpdate(id, {allow: true}) 
+            const user = await Users.findByIdAndUpdate(id, { allow: true })
 
-            if(!user) return res.status(404).send({message: "User not found"})
+            if (!user) return res.status(404).send({ message: "User not found" })
 
-            res.status(200).send({message: "User allowed to exams"})
+            res.status(200).send({ message: "User allowed to exams" })
         } catch (error) {
             console.error(error)
-            res.status(500).send({message: "Something went wrong"})
+            res.status(500).send({ message: "Something went wrong" })
         }
     },
-    unAllowUser: async (req,res) => {
+    unAllowUser: async (req, res) => {
         try {
-            const {id} = req.params;
+            const { id } = req.params;
 
-            const user = await Users.findByIdAndUpdate(id, {allow: false}) 
+            const user = await Users.findByIdAndUpdate(id, { allow: false })
 
-            if(!user) return res.status(404).send({message: "User not found"})
+            if (!user) return res.status(404).send({ message: "User not found" })
 
-            res.status(200).send({message: "User unallowed to exams"})
+            res.status(200).send({ message: "User unallowed to exams" })
         } catch (error) {
             console.error(error)
-            res.status(500).send({message: "Something went wrong"})
+            res.status(500).send({ message: "Something went wrong" })
+        }
+    },
+    viewHistory: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const user = await Users.findById(id);
+            const history = user.history;
+            res.status(200).send(history)
+        }
+        catch (error) {
+            console.error(error)
+            res.status(500).send({ message: "Something went wrong" })
+        }
+    },
+    addHistory: async (req, res) => {
+        try {
+            const {id} = req.params;
+            const body = req.body;
+            const user = await Users.findByIdAndUpdate(id, {history: {$push: body}});
+            if(!user) return res.status(404).send({message: "User not found"})
+            res.status(200).send(user)
+        } catch (error) {
+            console.error(error)
+            res.status(500).send({ message: "Something went wrong" })
         }
     }
 
